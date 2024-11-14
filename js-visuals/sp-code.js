8726b27913c22c3b79d55a6a2b6a2c201cd76b60
alyssakalbus <118205859+alyssakalbus@users.noreply.github.com>
2024-11-14 14:29:36 -0500

    Updated sp-code.js

--- js-visuals/sp-code.js
+++ js-visuals/sp-code.js
@@ -1,5 +1,7 @@
 export function spCode() {
     return `
-        box(vec3(.5));
+      float shape = sdBox(vec3(p.x, p.y, p.z), vec3(1.0, 1.0, 1.0));
+      vec3 color = vec3(0.2, 0.8, 1.0);
+      return vec4(color, 1.0);
     `;
   }
