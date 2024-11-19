4e2709ed5a2ac8bb743ea11ec3cad1ab99ea2b8f
Justin <justin@jjohnson.io>
2024-11-19 14:09:56 -0500

    YAY!

--- js-visuals/sp-code.js
+++ js-visuals/sp-code.js
@@ -5,14 +5,6 @@
 
 export function spCode() {
   // Put your Shader Park Code here
-  rotateY((mouse.x * PI) / 2 + time * 0.5);
-  rotateX((mouse.y * PI) / 2);
-  metal(0.5);
-  shine(0.4);
-  color(getRayDirection() + 0.2);
-  rotateY(getRayDirection().y * 4 + time);
-  boxFrame(vec3(0.4), 0.02);
-  expand(0.02);
-  blend(nsin(time) * 0.6);
-  sphere(0.2);
+  color(vec3(0.2, 0.8, 1.0));
+  box(vec3(0.4, 0.4, 0.4));
 }
