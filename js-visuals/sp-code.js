1d4857a178f15f12b45f8724bdf44d3914afcbcf
Justin <justin@jjohnson.io>
2024-11-19 11:29:09 -0500

    add shader-park example

--- js-visuals/sp-code.js
+++
@@ -1,7 +0,0 @@
-export function spCode() {
-    return `
-      float shape = sdBox(vec3(p.x, p.y, p.z), vec3(1.0, 1.0, 1.0));
-      vec3 color = vec3(0.2, 0.8, 1.0);
-      return vec4(color, 1.0);
-    `;
-  }
