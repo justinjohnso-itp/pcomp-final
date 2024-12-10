cb6b0ab52bc0d5fc63b7d41c37e34bdf52186fb9
alyssakalbus <118205859+alyssakalbus@users.noreply.github.com>
2024-12-10 09:08:16 -0500

    new new

--- CapacitiveSensor/CapacitiveSensor.ino
+++ CapacitiveSensor/CapacitiveSensor.ino
@@ -1,10 +1,15 @@
 #include <CapacitiveSensor.h>
 
-CapacitiveSensor cs_4_2 = CapacitiveSensor(4, 2);
+CapacitiveSensor cs_4_2 = CapacitiveSensor(4, 2); 
 CapacitiveSensor cs_5_3 = CapacitiveSensor(5, 3);
 
 
 const long threshold = 1000;
+
+bool buttonState1 = false;
+bool buttonState2 = false;
+bool buttonVal1 = false;
+bool buttonVal2 = false;
 
 void setup() {
 
@@ -20,11 +25,25 @@ void loop() {
   long total1 = cs_4_2.capacitiveSensor(30);
   long total2 = cs_5_3.capacitiveSensor(30);
 
-  if (total1 > threshold) {
-    Serial.println("Button 1 Pressed");
+  buttonVal1 = (total1 > threshold);
+  buttonVal2 = (total2 > threshold);
+
+  if (buttonVal1 != buttonState1) {
+    buttonState1 = buttonVal1;
+    if (!buttonState1) {
+      toggleState1 = !toggleState1;
+    }
+    toggleState1 ? controlChange(0, 6, 127) : controlChange(0, 7, 127);
   }
-  if (total2 > threshold) {
-    Serial.println("Button 2 Pressed");
+
+  if (buttonVal2 != buttonState2) {
+    buttonState2 = buttonVal2;
+    if (!buttonState2) {
+      toggleState2 = !toggleState2;
+    }
+    toggleState2 ? controlChange(0, 8, 127) : controlChange(0, 9, 127);
+  }
+
   }
 
 }
