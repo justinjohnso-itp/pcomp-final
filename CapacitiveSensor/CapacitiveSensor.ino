c8c6df847a35d8f1fdbc5103005975d2bfb295b3
alyssakalbus <118205859+alyssakalbus@users.noreply.github.com>
2024-12-10 08:20:33 -0500

    Update CapacitiveSensor.ino

--- CapacitiveSensor/CapacitiveSensor.ino
+++ CapacitiveSensor/CapacitiveSensor.ino
@@ -1,32 +1,30 @@
 #include <CapacitiveSensor.h>
 
-CapacitiveSensor cs_4_2 = CapacitiveSensor(4,2); // 10 megohm resistor between pins 4 & 2, pin 2 is sensor pin, add wire, foil
-CapacitiveSensor cs_4_2 = CapacitiveSensor(5,3);
+CapacitiveSensor cs_4_2 = CapacitiveSensor(4, 2); // 10M resistor between pins 4 & 2
+CapacitiveSensor cs_5_3 = CapacitiveSensor(5, 3); // 10M resistor between pins 5 & 3
 
-void setup(){
 
-cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF); // turn off autocalibrate on channel 1 - just as an example Serial.begin(9600);
-cs_5_3.set_CS_AutocaL_Millis(0xFFFFFFFF); 
+const long threshold = 1000;
 
+void setup() {
+
+  cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);
+  cs_5_3.set_CS_AutocaL_Millis(0xFFFFFFFF);
+
+
+  Serial.begin(9600);
 }
 
-void loop(){
+void loop() {
+  // Read values
+  long total1 = cs_4_2.capacitiveSensor(30);
+  long total2 = cs_5_3.capacitiveSensor(30);
 
-long start = millis();
-
-long total1 = cs_4_2.capacitiveSensor(30);
-long total1 = cs_5_3.capacitiveSensor(30);
-
-Serial.print(millis() - start); // check on performance in milliseconds
-
-Serial.print("\t"); // tab character for debug window spacing
-
-Serial.print(total1); // print sensor output 1
-
-Serial.print("\t"); // tab character for debug window spacing
-
-Serial.print(total2); // print sensor output 1
-
-delay(10); // arbitrary delay to limit data to serial port
+  if (total1 > threshold) {
+    Serial.println("Button 1 Pressed");
+  }
+  if (total2 > threshold) {
+    Serial.println("Button 2 Pressed");
+  }
 
 }
